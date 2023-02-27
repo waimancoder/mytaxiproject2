d0aa8d1c6b30cd4b8a6a12aa8efbc395b1010ee0
waimancoder <121964767+waimancoder@users.noreply.github.com>
2023-02-28 04:21:02 +0800

    Update urls.py

--- rides/urls.py
+++ rides/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.urls import re_path
 from . import views
 from .views import RequestDriverView, DriverApprovalView, LocationDetailView
 from django.contrib.auth import get_user_model
@@ -7,6 +8,6 @@ urlpatterns = [
     path('api/requestdriver', RequestDriverView.as_view(), name='requestdriver'),
     path('api/driverapproval', DriverApprovalView.as_view(), name='driverapproval'),
     path('api/locations', LocationDetailView.as_view(), name='location-list'),
-    path('api/locations/(?P<name>[\w\s]+)/$', LocationDetailView.as_view(), name='location-detail')
+    re_path(r'^api/locations/(?P<name>[\w\s]+)/$', LocationDetailView.as_view(), name='location-detail'),
 
 ]
