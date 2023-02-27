1af81d6e0a57a861e63ef0a3264106e813652d57
waimancoder <121964767+waimancoder@users.noreply.github.com>
2023-02-28 04:16:54 +0800

    Update urls.py

--- rides/urls.py
+++ rides/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('api/requestdriver', RequestDriverView.as_view(), name='requestdriver'),
     path('api/driverapproval', DriverApprovalView.as_view(), name='driverapproval'),
     path('api/locations', LocationDetailView.as_view(), name='location-list'),
-    path('api/locations/<str:name>/', LocationDetailView.as_view(), name='location-detail')
+    path('api/locations/(?P<name>[\w\s]+)/$', LocationDetailView.as_view(), name='location-detail')
 
 ]
