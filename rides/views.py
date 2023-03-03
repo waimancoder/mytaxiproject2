849c62958e110964f6bbdd0b9056d3ebcd588384
waimancoder <waimancoder@gmail.com>
2023-03-03 21:50:09 +0800

    Update views.py

--- rides/views.py
+++ rides/views.py
@@ -58,5 +58,23 @@ class LocationDetailView(generics.RetrieveUpdateAPIView, mixins.ListModelMixin,m
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        response_data = {'status':"CREATED",'statusCode': status.HTTP_201_CREATED, 'data': serializer.data}
+        response_data = {
+            'status':"CREATED",
+            'statusCode': status.HTTP_201_CREATED, 
+            'data': serializer.data
+            }
         return Response(response_data, status=status.HTTP_201_CREATED)
+    
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        response_data = {
+            'status': "OK",
+            'statusCode': status.HTTP_200_OK,
+            'data': serializer.data
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
