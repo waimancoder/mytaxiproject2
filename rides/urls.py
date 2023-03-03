d6a636186b67d65eb5d355335a1f84f0ae901fd3
waimancoder <waimancoder@gmail.com>
2023-03-03 20:47:26 +0800

    Update urls.py

--- rides/urls.py
+++ rides/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import RequestDriverView, DriverApprovalView, LocationDetailView
+from .views import LocationDetailView
 from django.contrib.auth import get_user_model
 
 urlpatterns = [
