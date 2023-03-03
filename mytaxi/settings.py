682ce6d8d125bf26a6bf107261b44be78cf66693
waimancoder <waimancoder@gmail.com>
2023-03-03 23:30:13 +0800

    Update settings.py

--- mytaxi/settings.py
+++ mytaxi/settings.py
@@ -196,6 +196,17 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_METHODS = [ 'DELETE',    'GET',    'OPTIONS',    'PATCH',    'POST',    'PUT',]
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000/"]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
 
 #Email Settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
