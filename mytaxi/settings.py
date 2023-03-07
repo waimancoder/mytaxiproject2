b7799e0a76f2f2edb78e71b3b446d4701983cf48
waimancoder <waimancoder@gmail.com>
2023-03-07 17:23:49 +0800

    Update settings.py

--- mytaxi/settings.py
+++ mytaxi/settings.py
@@ -105,22 +105,22 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR +'/db.sqlite3',
     # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'pgdb',
-        'PORT': 5432,
-    }
     # 'default': {
-    # 'ENGINE': 'django.db.backends.postgresql',
-    # 'NAME': os.environ.get('dbname'),
-    # 'USER': os.environ.get('dbuser'),
-    # 'PASSWORD': os.environ.get('dbpassword'),
-    # 'HOST': os.environ.get('dbhost'),
-    # 'PORT': 5432,
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'postgres',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST': 'pgdb',
+    #     'PORT': 5432,
     # }
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.environ.get('dbname'),
+    'USER': os.environ.get('dbuser'),
+    'PASSWORD': os.environ.get('dbpassword'),
+    'HOST': os.environ.get('dbhost'),
+    'PORT': 5432,
+    }
 }
 
 
