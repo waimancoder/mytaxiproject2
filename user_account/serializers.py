68929c0fb346ca9e003cb311eca6fabcbf6e6481
waimancoder <waimancoder@gmail.com>
2023-03-07 21:12:21 +0800

    Update serializers.py

--- user_account/serializers.py
+++ user_account/serializers.py
@@ -74,7 +74,7 @@ class RegisterSerializer(serializers.ModelSerializer):
             img_temp = tempfile.NamedTemporaryFile(delete=True)
             img_temp.write(response.content)
             img_temp.flush()
-            user.profile_img.save(f'/pic.png', File(img_temp), save=True)
+            user.profile_img.save('pic.png', File(img_temp), save=True)
         
         user.save()
 
